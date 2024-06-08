81afaa2615c58fe296ecfece602545fec993d02c
GiuseppeNovielli <giuseppe.novielli@hrcoffee.it>
2024-06-08 13:35:41 +0200

    move nested create logic to serializer **MORE DRY**

--- src/motorizations/serializers.py
+++ src/motorizations/serializers.py
@@ -25,16 +25,57 @@ class CarSerializer(ValidateModelSerializer, serializers.ModelSerializer):
         representation['engine'] = EngineSerializer(instance.engine).data
         return representation
     
-
+    
+    def is_valid(self, raise_exception=False, **kwargs):
+        is_valid = super().is_valid(raise_exception, **kwargs)
+        if not is_valid:
+            return is_valid
+        
+        self.car_users_is_valid()
+        return is_valid
+    
+    
+    def create(self, validated_data):
+        car = super().create(validated_data)
+        self.car_users_create(car)
+        return car
+    
+    
+    def car_users_is_valid(self):
+        if self.instance:
+            return
+        
+        car = self.Meta.model(**self.validated_data)
+        
+        context = self.context
+        request = context['request']
+        
+        serializers_car_user = []
+        for car_user_dict in request.data.get('car_users', []):
+            s = CarUser_CarExcluded_Serializer(data=car_user_dict, context=context)
+            s.is_valid(raise_exception=True, car=car)
+            serializers_car_user.append(s)
+        context['car_user_serialzers'] = serializers_car_user
+        
+    
+    def car_users_create(self, car):
+        car_users_serializers = self.context.get('car_user_serialzers', [])
+        for s in car_users_serializers:
+            s.save(car=car)
+     
+            
+#
+   
+   
 #CarUser
 class CarUserSerializer(ValidateModelSerializer, serializers.ModelSerializer):
-        
+    
     class Meta:
         model = CarUser
         fields = ('id', 'user', 'car', 'number_plate',)
         #https://www.django-rest-framework.org/api-guide/serializers/#specifying-read-only-fields
         read_only_fields = ['user_created', 'datetime_created']
-      
+        
     def to_representation(self, instance):
         """
         https://testdriven.io/tips/ed79fa08-6834-4827-b00d-2609205129e0/
@@ -57,17 +98,50 @@ class CarUserSerializer(ValidateModelSerializer, serializers.ModelSerializer):
         
         #Set the read_only fields values
         attrs['user_created'] = request.user
-        attrs['datetime_created'] = timezone.now()
         return super().validate(attrs)
     
     
-class CarUserExcludeCarSerializer(CarUserSerializer):
+class CarUser_CarExcluded_Serializer(CarUserSerializer):
         
-    class Meta(CarUserSerializer.Meta):
-        fields = ('user', 'number_plate',)
-      
+    class Meta:
+        model = CarUser
+        fields = ('pk', 'user', 'number_plate',)
+        
+
+class CarUser_CarNested_Serializer(CarUser_CarExcluded_Serializer):
+    
+    def validate(self, attrs):
+        attrs = super().validate(attrs)
+        
+        context = self.context
+        if context.get('car'):
+            return attrs
+        
+        s = CarSerializer(data=attrs.get('car'), context=self.context)
+        s.is_valid(raise_exception=True)
+        context['car'] = Car(**s.validated_data)
+        
+        return attrs
+    
+    def is_valid(self, raise_exception=False, **kwargs):
+        self.validate(self.initial_data)
+        
+        context = self.context
+        car = context['car']
+        
+        return super().is_valid(raise_exception, car=car, **kwargs)
+    
+    
+    def create(self, validated_data):
+        car = self.context['car']
+        car.save()
+        validated_data['car'] = car
+        
+        return super().create(validated_data)
+
 
 #
+
 
 #WRITEBLE NESTED SERIALIZER
 class Engine_writable_nested_Serializer(WritableNestedModelSerializer): 
@@ -76,21 +150,77 @@ class Engine_writable_nested_Serializer(WritableNestedModelSerializer):
         fields = ['pk', 'name']
         
 
-class Car_writable_nested_Serializer(WritableNestedModelSerializer): 
+
+class Car_CarUserWritableNestedSerializer(WritableNestedModelSerializer):
+    """
+    Internal Server Error: /motorizations/api/cars/
+Traceback (most recent call last):
+  File "/Users/hrcoffee6/my_venv/django_3/lib/python3.9/site-packages/django/core/handlers/exception.py", line 47, in inner
+    response = get_response(request)
+  File "/Users/hrcoffee6/my_venv/django_3/lib/python3.9/site-packages/django/core/handlers/base.py", line 181, in _get_response
+    response = wrapped_callback(request, *callback_args, **callback_kwargs)
+  File "/Users/hrcoffee6/my_venv/django_3/lib/python3.9/site-packages/django/views/decorators/csrf.py", line 54, in wrapped_view
+    return view_func(*args, **kwargs)
+  File "/Users/hrcoffee6/my_venv/django_3/lib/python3.9/site-packages/rest_framework/viewsets.py", line 125, in view
+    return self.dispatch(request, *args, **kwargs)
+  File "/Users/hrcoffee6/my_venv/django_3/lib/python3.9/site-packages/rest_framework/views.py", line 509, in dispatch
+    response = self.handle_exception(exc)
+  File "/Users/hrcoffee6/my_venv/django_3/lib/python3.9/site-packages/rest_framework/views.py", line 469, in handle_exception
+    self.raise_uncaught_exception(exc)
+  File "/Users/hrcoffee6/my_venv/django_3/lib/python3.9/site-packages/rest_framework/views.py", line 480, in raise_uncaught_exception
+    raise exc
+  File "/Users/hrcoffee6/my_venv/django_3/lib/python3.9/site-packages/rest_framework/views.py", line 506, in dispatch
+    response = handler(request, *args, **kwargs)
+  File "/Users/hrcoffee6/django_dry_project/src/motorizations/views.py", line 44, in create
+    return super().create(request, *args, **kwargs)
+  File "/Users/hrcoffee6/my_venv/django_3/lib/python3.9/site-packages/rest_framework/mixins.py", line 19, in create
+    self.perform_create(serializer)
+  File "/Users/hrcoffee6/my_venv/django_3/lib/python3.9/site-packages/rest_framework/mixins.py", line 24, in perform_create
+    serializer.save()
+  File "/Users/hrcoffee6/my_venv/django_3/lib/python3.9/site-packages/drf_writable_nested/mixins.py", line 234, in save
+    return super(BaseNestedModelSerializer, self).save(**kwargs)
+  File "/Users/hrcoffee6/my_venv/django_3/lib/python3.9/site-packages/rest_framework/serializers.py", line 212, in save
+    self.instance = self.create(validated_data)
+  File "/Users/hrcoffee6/my_venv/django_3/lib/python3.9/site-packages/drf_writable_nested/mixins.py", line 262, in create
+    self.update_or_create_reverse_relations(instance, reverse_relations)
+  File "/Users/hrcoffee6/my_venv/django_3/lib/python3.9/site-packages/drf_writable_nested/mixins.py", line 166, in update_or_create_reverse_relations
+    instances = self._prefetch_related_instances(field, related_data)
+  File "/Users/hrcoffee6/my_venv/django_3/lib/python3.9/site-packages/drf_writable_nested/mixins.py", line 128, in _prefetch_related_instances
+    pk_list = self._extract_related_pks(field, related_data)
+  File "/Users/hrcoffee6/my_venv/django_3/lib/python3.9/site-packages/drf_writable_nested/mixins.py", line 120, in _extract_related_pks
+    pk = self._get_related_pk(d, model_class)
+  File "/Users/hrcoffee6/my_venv/django_3/lib/python3.9/site-packages/drf_writable_nested/mixins.py", line 109, in _get_related_pk
+    pk = data.get('pk') or data.get(model_class._meta.pk.attname)
+AttributeError: 'str' object has no attribute 'get'
+    """
     #engine = Engine_writable_nested_Serializer()
-    car_user = CarUserSerializer(many=True)
+    car_user = CarUser_CarExcluded_Serializer()
 
     class Meta:
         model = Car
-        fields = ('id', 'name', 'engine', 'car_user',)
+        fields = ('pk', 'name', 'engine', 'car_user',)
 
 
-class Car_user_writable_nested_Serializer(WritableNestedModelSerializer):
-    car = Car_writable_nested_Serializer()
+
+class CarUser_WritableNestedSerializer(WritableNestedModelSerializer):
+    """
+    ISSUE
+    If Car_User.clean() raise an exception, but Car.clean() doesn't, Car will be saved into db but without Car_User
+    WORKAROUND
+    use -> CarUser_CarNested_Serializer
+    """
+    car = CarSerializer()
     
     class Meta:
         model = CarUser
-        fields = ['id', 'user', 'number_plate']
+        fields = ('id', 'user', 'car', 'number_plate',)
+        read_only_fields = ['user_created', 'datetime_created']
+    
+    
+    def validate(self, attrs):
+        request = self.context['request']
         
-      
+        #Set the read_only fields values
+        attrs['user_created'] = request.user
+        return super().validate(attrs)
     
--- src/motorizations/views.py
+++ src/motorizations/views.py
@@ -3,11 +3,8 @@ from rest_framework import viewsets, filters
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Engine, Car, CarUser
-from .serializers import CarUserExcludeCarSerializer, EngineSerializer, CarSerializer, CarUserSerializer
+from .serializers import Car_CarUserWritableNestedSerializer, CarUser_CarNested_Serializer, CarUser_WritableNestedSerializer, EngineSerializer, CarSerializer, CarUserSerializer
 from .filters import EngineFilter, CarFilter
-
-from rest_framework import status
-from rest_framework.response import Response
 
 class EngineViewSet(viewsets.ModelViewSet):
     serializer_class = EngineSerializer
@@ -17,37 +14,16 @@ class EngineViewSet(viewsets.ModelViewSet):
     
 
 class CarViewSet(viewsets.ModelViewSet):
-    serializer_class = CarSerializer
     filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
     filterset_class = CarFilter
     queryset = Car.objects.all()
-    
-    def _writable_many_car_user(self, request):
-        s_context = self.get_serializer_context()
-        
-        s_car = self.get_serializer(data=request.data,context=s_context)
-        s_car.is_valid(raise_exception=True)
-        car = Car(**s_car.validated_data)
-        
-        serializers_car_user = []
-        for car_user_dict in request.data.get('car_user', []):
-            s_car_user = CarUserExcludeCarSerializer(data=car_user_dict, context=s_context)
-            s_car_user.is_valid(raise_exception=True, car=car)
-            serializers_car_user.append(s_car_user)
-        
-        car = s_car.save()
-        for s_car_user in serializers_car_user:
-            s_car_user.save(car=car)
-        
-        respose_data=s_car.data
-        return Response(respose_data, status=status.HTTP_201_CREATED, headers=self.get_success_headers(respose_data))
-        
-        
+
+
     def create(self, request, *args, **kwargs):
         """
         Test with POSTMAN
 
-        curl --location 'http://127.0.0.1:8000/motorizations/api/cars/?writable_car_user=true' \
+        curl --location 'http://127.0.0.1:8000/motorizations/api/cars/' \
 --header 'Authorization: Token be4126fdd668feda4c4a4c9b7761d5af15c1dee3' \
 --header 'Content-Type: application/json' \
 --data '{
@@ -60,22 +36,45 @@ class CarViewSet(viewsets.ModelViewSet):
         },
         {
             "user": 1,
-            "number_plate": "admin345"
+            "number_plate": "admin456"
         }
     ]
 }'
         """
-        if request.query_params.get('writable_car_user'):
-            return self._writable_many_car_user(request)
         return super().create(request, *args, **kwargs)
     
-    
-    def destroy(self, request, *args, **kwargs):
-        return super().destroy(request, *args, **kwargs)
-        
+    def get_serializer_class(self):
+        return CarSerializer
+        #return Car_CarUserWritableNestedSerializer
+      
     
 class CarUserViewSet(viewsets.ModelViewSet):
-    serializer_class = CarUserSerializer
     filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
     filterset_fields = '__all__'
     queryset = CarUser.objects.all()
+    
+    def get_serializer_class(self):
+        car = self.request.data.get('car')
+        if car and isinstance(car, dict):
+            return CarUser_CarNested_Serializer
+            #return CarUser_WritableNestedSerializer
+        return CarUserSerializer
+    
+    
+    def create(self, request, *args, **kwargs):
+        """
+        Test with POSTMAN
+
+        curl --location 'http://127.0.0.1:8000/motorizations/api/cars-users/' \
+--header 'Authorization: Token be4126fdd668feda4c4a4c9b7761d5af15c1dee3' \
+--header 'Content-Type: application/json' \
+--data '{
+    "car": {
+            "name": "advanced-electric",
+            "engine": "1"
+        },
+    "user" : 1,
+    "number_plate" : "admin"
+}'
+        """
+        return super().create(request, *args, **kwargs)
